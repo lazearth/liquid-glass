5f5cb4c14a492c9ca035caf3a10b9347f66ef512
PHREERAPHAT BOONWIANG <phreeraphat.bo@gmail.com>
2026-05-07 10:46:49 +0700

    Update box-shadow and backdrop-filter styles
    Added the white inner rim glow and boosted the brightness to 1.5

--- liquid-glass.js
+++ liquid-glass.js
@@ -65,9 +65,9 @@
         height: ${this.height}px;
         overflow: hidden;
         border-radius: 150px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.25), 0 -10px 25px inset rgba(0, 0, 0, 0.15);
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.25), 0 -10px inset rgba(0, 0, 0, 0.15), 0 -1px 4px 1px inset rgba(255, 255, 255, 0.74);
         cursor: grab;
-        backdrop-filter: url(#${this.id}_filter) blur(0.25px) contrast(1.2) brightness(1.05) saturate(1.1);
+        backdrop-filter: url(#${this.id}_filter) blur(0.25px) brightness(1.5) saturate(1.1);
         z-index: 9999;
         pointer-events: auto;
       `;
